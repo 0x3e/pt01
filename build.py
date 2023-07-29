27dabd894ab09dda2e50442ebd7ad6d4d3097cb7
0x3e <0x3e.com@gmail.com>
2023-07-30 09:55:52 +1000

    .

---
+++ build.py
@@ -0,0 +1,27 @@
+import os
+import shutil
+from subprocess import check_output
+
+def run_cmd(cmd_str):
+  print(cmd_str)
+  print(check_output(cmd_str + " ", shell=True).decode())
+  return
+
+table_file = '"PT01 (VPX 2023) 0x3e.vpx"'
+there_table_file = 'C:\\"Visual Pinball"\Tables\\' + table_file
+here_table_file = os.getcwd() + '\\' + table_file
+
+print('')
+print(there_table_file)
+#print(os.stat(there_table_file))
+print(here_table_file)
+#print(os.stat(here_table_file))
+#shutil.copyfile(there_table_file, here_table_file)  
+
+run_cmd('copy /y ' + there_table_file + ' ' + here_table_file)
+
+run_cmd('cd C:\Visual Pinball &&  VPinballX.exe -ExtractVBS ' + here_table_file)
+
+run_cmd('certutil -hashfile ' + here_table_file + ' MD5 > checksums.txt')
+
+run_cmd('certutil -hashfile ' + here_table_file + ' SHA256 >> checksums.txt')
